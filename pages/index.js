47d376ed00e3bae8009bdec79f5ef681732b7a56
[abdhmd] <[sirhmd@outlook.com]>
2021-05-27 15:37:13 +0100

    fix error

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Hero from "../sections/Hero";
 import Features from "../sections/Features";
 import About from "../sections/About";
 import Faq from "../sections/Faq";
-import Footer from "../sections/Footer";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
